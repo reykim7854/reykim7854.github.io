bafa63371664f962dd4e44d8ea01cb2b0b0cf0cc
mrayhanhakim <rayhanhakim@rayhan-pc.local>
2019-04-19 10:32:32 +0700

    Change navbar bg-color and remove bg-img on about page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,42 +3,15 @@ document.body.onload = function onLoad() {
   const page = document.getElementsByClassName('content');
   const link = document.getElementsByClassName('nav-link');
   const footer = document.getElementById('footer');
-  const nav = document.getElementById('navbar');
   const navToggle = document.getElementById('nav-toggle');
   const navDrop = document.getElementById('right-nav-link');
-  const footerLink = document.getElementsByClassName('footer-link');
   for (let i = 0; i < link.length; i += 1) {
     link[i].addEventListener('click', () => {
       page[i].classList.add('active');
       if (page[i].id !== 'home') {
         footer.classList.remove('sticky');
-        footer.classList.add('textShadow');
-        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-        if (navDrop.classList.contains('drop')) {
-          navDrop.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
-        }
-        navToggle.style.color = 'white';
-        navToggle.classList.add('textShadow');
-        document.body.style.background = 'url(../../assets/images/about/bg.png) no-repeat center center fixed';
-        document.body.style.backgroundSize = 'cover';
-        for (let k = 0; k < link.length; k += 1) {
-          link[k].style.color = 'white';
-          link[k].classList.add('textShadow');
-          footerLink[k].style.color = 'white';
-        }
       } else {
         footer.classList.add('sticky');
-        footer.classList.remove('textShadow');
-        document.body.style.background = 'transparent';
-        nav.style.backgroundColor = 'transparent';
-        navDrop.style.backgroundColor = 'transparent';
-        navToggle.style.color = 'black';
-        navToggle.classList.remove('textShadow');
-        for (let k = 0; k < link.length; k += 1) {
-          link[k].style.color = 'black';
-          link[k].classList.remove('textShadow');
-          footerLink[k].style.color = 'black';
-        }
       }
       for (let j = 0; j < page.length; j += 1) {
         if (page[j].classList.contains('active') && page[i] !== page[j]) {
